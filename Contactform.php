bb8ff253d98a2f18fa457b5de5850d9b624c2898
TarekMo <38524173+Tarekmohammed109@users.noreply.github.com>
2018-04-20 15:53:12 +0200

    Project

--- Contactform.php
+++ Contactform.php
@@ -1,16 +1,23 @@
 <?php
-    if (isset($_POST['submit']))
-    {
+
         $name = $_POST['name'];
-        $subject = $_POST['subject'];
-        $mailFrom = $_POST['mail'];
+        $visitor_email = $_POST['mail'];
         $message = $_POST['message'];
         
-        $mailTo = "tarekmohammed109@yahoo.com";
-        $headers = "From: ".$mailFrom;
-        $txt = "You have recieved an email from ".$name.".\n\n".$message;
+        $email_from 'tarekmohammed@outlook.com';
         
-        mail($mailTo, $subject, $txt, $headers);
+        $email_subject = "New Form Submission";
+        
+        $email_body = "User Name: $name.\n".
+                        "User Email: $visitor_email.\n".
+                            "User Message: $message/\n";
+            
+        $to = "tarekmohammed109@yahoo.com";
+
+        $headers = "From: $email_from \r\n";
+
+        $headers .="Reply-To: $visitor_mail \r\n";
+        
+        mail($to,$email_subject,$email_body,$headers);
         header("Location: Contact.html");
-    }
 ?>
